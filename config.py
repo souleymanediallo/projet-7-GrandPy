3e3e93feaf11cedc40f011f364a6b00c8ff891ea
souleymane <souleymanedialloo@yahoo.fr>
2021-04-13 17:44:46 +0200

    update

--- config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY_API_GOOGLE = "AIzaSyC3taP9K0zXslYQWg_AXQs11XTZKmJkYkA"
-
-GOOGLE_API_URL = "https://maps.googleapis.com/maps/api/geocode/json"
-
-MEDIA_WWIKIPEDIA_URL = "https://fr.wikipedia.org/w/api.php"
-
-MEDIAWIKI_API_URL = "http://fr.wikipedia.org/w/api.php"
-
